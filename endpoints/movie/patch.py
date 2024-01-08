5ad60930d474eeff9c11eeeaf65da2d611e091fe
JesperSchuurman <jesper.schuurman@student.nhlstenden.com>
2024-01-08 15:19:25 +0100

    changed delete_movie to work with id and started with patch_movie endpoint

--- endpoints/movie/patch.py
+++ endpoints/movie/patch.py
@@ -1,19 +1,20 @@
 from fastapi.responses import JSONResponse
 from fastapi import Form
+import cv2
 
 from src import App, Endpoint, Method
 
-class DeleteMovie(Endpoint):
+class PatchMovie(Endpoint):
 
-    async def callback(self, id: int = Form()) -> JSONResponse:
+    async def callback(self, id: int = Form(), title: str = Form(), filepath: str = Form(), genre_id: int = Form()) -> JSONResponse:
 
         async with self.pool.acquire() as db:
             async with db.cursor() as cursor:
                 await cursor.execute("SELECT id FROM Movie where movie WHERE id = %s", (id,))
                 if await cursor.fetchone():
                     return JSONResponse({"error": "This movie does not exist."}, 400)
-                await cursor.execute("DELETE FROM Movie WHERE id = %s", (id,))
+                await cursor.execute("UPDATE title = %s, filepath = %s, ")
                 return JSONResponse({})
             
-def setup(app : App) -> DeleteMovie:
-    return DeleteMovie(app, Method.DELETE, "/movie/delete", JSONResponse)
+def setup(app : App) -> PatchMovie:
+    return PatchMovie(app, Method.PATCH, "/movie/patch", JSONResponse)
