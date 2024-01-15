5344eee1f41ae98daeecde8fed5b93a34e4490cb
Raeven <kasetoatz@gmail.com>
2024-01-15 15:05:27 +0100

    Update login.py

--- endpoints/user/login.py
+++ endpoints/user/login.py
@@ -13,7 +13,7 @@ class RegisterUser(Endpoint):
     async def callback(self, email: str = Form(), password: str = Form()) -> JSONResponse:
         async with self.app.pool.acquire() as db:
             async with db.cursor() as cursor:
-                await cursor.execute("SELECT id, password FROM Account WHERE email = %s", (email,))
+                await cursor.execute("SELECT email, password FROM Account WHERE email = %s", (email,))
                 result = await cursor.fetchone()
                 if not result:
                     return JSONResponse({"error": "User with this email does not exist."}, 400)
