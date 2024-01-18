5cb1c0cacb993f22956f4c5b4a98f94b52ebc5b2
Raeven <kasetoatz@gmail.com>
2024-01-18 10:35:48 +0100

    Update getAll.py

--- endpoints/movie/getAll.py
+++ endpoints/movie/getAll.py
@@ -14,7 +14,7 @@ class GetAllMovies(Endpoint):
                 if not movies:
                     return JSONResponse({"error": "There aren't any movies yet."}, 400)
                 movie_list = [{"id": movie[0], "title": movie[1]} for movie in movies]
-                return JSONResponse({movie_list})
+                return JSONResponse(movie_list)
             
 def setup(app : App) -> GetAllMovies:
     return GetAllMovies(app, Method.GET, "/movie/getAll", JSONResponse)
