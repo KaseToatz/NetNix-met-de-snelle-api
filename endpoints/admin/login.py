343ac841b6f35f2f2a6ecceef785431aa9e738a1
Raeven <kasetoatz@gmail.com>
2024-01-18 10:06:55 +0100

    admin login fixes

--- endpoints/admin/login.py
+++ endpoints/admin/login.py
@@ -17,8 +17,7 @@ class LoginAdmin(Endpoint):
                 await cursor.execute("SELECT email, password FROM Admin WHERE email = %s", (email,))
                 result = await cursor.fetchone()
                 if not result:
-                    return JSONResponse({"error": "User with this email does not exist."}, 400)
-                print(result)
+                    return JSONResponse({"error": "Admin with this email does not exist."}, 400)
                 if not self.app.pwdContext.verify(password, result[1]):
                     return JSONResponse({"error": "Password does not match."}, 401)
                 return JSONResponse({"token": jwt.encode({"sub": result[0], "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, SIGNING_KEY)})
--- hash.py
+++
@@ -1,7 +0,0 @@
-from passlib.context import CryptContext
-
-ctx = CryptContext(schemes=["bcrypt"])
-
-yes = ctx.hash("bestebaas")
-
-print(yes)
