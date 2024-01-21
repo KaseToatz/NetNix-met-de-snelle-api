94571bc6a3c929de371015e332fa51b45f5d8ea4
JesperSchuurman <jesper.schuurman@student.nhlstenden.com>
2024-01-21 20:35:22 +0100

    changed getMonthlyProfits to include the stored procedure

--- endpoints/user/getMonthlyProfits.py
+++ endpoints/user/getMonthlyProfits.py
@@ -8,23 +8,13 @@ class GetMonthlyProfits(Endpoint):
 
         async with Connection(UserType.JUNIOR) as db:
             async with db.cursor() as cursor:
-                await cursor.execute("SELECT Subscription, referd_by_acount FROM Acount")
-                acounts = await cursor.fetchall()
-                if not acounts:
-                    return JSONResponse({"error": "There aren't any acounts yet."}, 400)
+                await cursor.callproc("get_subscription_data") 
+                results = await cursor.fetchall()
+                if not results:
+                    return JSONResponse({"error": "There aren't any subscriptions yet."}, 400)
                 profits = 0
-                for acount in acounts: 
-                    subscription = acount[0]
-                    referd_by_acount = acount[1]
-                    if subscription != 0:
-                        if subscription == 1:
-                            profits += 7.99
-                        elif subscription == 2:
-                            profits += 10.99
-                        elif subscription == 3:
-                            profits +=13.99
-                        if referd_by_acount != 0:
-                            profits -= 2       
+                for result in results:
+                    profits += result[3]
 
                 return JSONResponse({profits})
             
